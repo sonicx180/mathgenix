af61d7a02bd6eac7c42e87851a0f3f974982e299
sonicx180 <83236320+sonicx180@users.noreply.github.com>
2022-03-05 20:48:59 -0800

    Fix errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req,res) => {
-  res.sendFile(__dirname + "templates/index.html);
+  res.sendFile(__dirname + "/templates/index.html");
   
 })
 
